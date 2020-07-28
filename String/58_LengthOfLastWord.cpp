69119bbecd53bffe7acc9e27ad68d845d78cc178
ChinaParry <1378805815@qq.com>
2020-07-28 23:39:22 +0800

    58 solved, very simple

---
+++ String/58_LengthOfLastWord.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Pengr on 2020/7/28.
+//
+
+//Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+//
+//If the last word does not exist, return 0.
+//
+//Note: A word is defined as a maximal substring consisting of non-space characters only.
+//
+//Example:
+//    Input: "Hello World"
+//    Output: 5
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        if(s.length()==0)
+            return len;
+        while(s.length()>0 && s[s.length()-1]==' ')
+            s = s.substr(0, s.length()-1);
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]!=' ')
+                len++;
+            else
+                break;
+        }
+        return len;
+    }
+};
